32a4ecbb84b55a436ddab5f4baf190ad1ba55a4c
Wei Wang <w2wang@mtu.edu>
2014-07-28 18:04:35 -0400

    fixed regexp bug

--- src/main/scala/com/mathworks/weiw/spark/examples/GroupedMeanApp.scala
+++ src/main/scala/com/mathworks/weiw/spark/examples/GroupedMeanApp.scala
@@ -1,5 +1,5 @@
 /**
- * Compute mean arrival delays grouped by airline.
+ * Compute mean arrival delays grouped by a grouping variable.
  * Created by wei wang on 7/28/14.
  */
 
@@ -11,12 +11,14 @@ import org.apache.spark._
 object GroupedMeanApp {
 
   def main(args: Array[String]) {
-    val filePath  = if (args.length > 0) args(0) else "/sandbox/bigdata/datasets/airline/2008.csv"
+    val grpIdx =  if (args.length > 0) args(0) else 3
+    val filePath  = if (args.length > 1) args(1) else "/sandbox/bigdata/datasets/airline/2008.csv"
     val conf = new SparkConf().setAppName("Spark Example: Grouped Mean")
     val sc = new SparkContext(conf)
-    val dataSet = sc.textFile(filePath).filter(line => line.split(",")(14).matches("""\d+"""))
-    val delaysRDD = dataSet.map((line:String) => {val f=line.split(","); (f(8), (f(14).toDouble, 1.0))})
-    val groupMean = delaysRDD.reduceByKey((a:(Double,Double),b:(Double,Double)) => (a._1 +b._1, a._2 + b._2)).map(x =>(x._1, x._2._1/x._2._2)).collect()
+    val dataSet = sc.textFile(filePath).filter(line => line.split(",")(14).matches("""[-+]?[0-9]*\.?[0-9]+"""))
+    val delaysRDD = dataSet.map((line:String) => {val f=line.split(","); (f(3), (f(14).toDouble, 1.0))})
+    println(delaysRDD.top(10))
+    val groupMean = delaysRDD.reduceByKey((a: (Double, Double), b: (Double, Double)) => (a._1 + b._1, a._2 + b._2)).map(x => (x._1, x._2._1 / x._2._2)).collect()
 
     println(groupMean.mkString("\n"))
   }
