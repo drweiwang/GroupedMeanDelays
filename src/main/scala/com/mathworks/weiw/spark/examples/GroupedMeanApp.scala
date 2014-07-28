69778494fd418de6a4976b84eb4252dd0164b71f
Wei Wang <w2wang@mtu.edu>
2014-07-28 15:31:44 -0400

    replace function getDelays with function literals

--- src/main/scala/com/mathworks/weiw/spark/examples/GroupedMeanApp.scala
+++ src/main/scala/com/mathworks/weiw/spark/examples/GroupedMeanApp.scala
@@ -1,10 +1,11 @@
 /**
- * Created by weiw on 7/28/14.
+ * Compute mean arrival delays grouped by airline.
+ * Created by wei wang on 7/28/14.
  */
 
 package com.mathworks.weiw.spark.examples
 
-import org.apache.spark.SparkConf
+//import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext._
 import org.apache.spark._
 
@@ -15,17 +16,9 @@ object GroupedMeanApp {
     val conf = new SparkConf().setAppName("Spark Example: Grouped Mean")
     val sc = new SparkContext(conf)
     val dataSet = sc.textFile(filePath).filter(line => line.split(",")(14).matches("""\d+"""))
-    val delaysRDD = dataSet.map(getDelays)
-    val groupMean = delaysRDD.reduceByKey((a:(Double,Double),b:(Double,Double)) => (a._1 +b._1, a._2 + b._2)).map(x =>(x._1, x._2._1/x._2._2)).collect
+    val delaysRDD = dataSet.map((line:String) => {val f=line.split(","); (f(8), (f(14).toDouble, 1.0))})
+    val groupMean = delaysRDD.reduceByKey((a:(Double,Double),b:(Double,Double)) => (a._1 +b._1, a._2 + b._2)).map(x =>(x._1, x._2._1/x._2._2)).collect()
 
-     println(groupMean.mkString("\n"))
-  }
-
-  def getDelays(line:String ): (String, (Double, Double)) ={
-    val fields = line.split(",")
-    val airline = fields(8)
-    val delay = fields(14)
-    (airline, (delay.toDouble, 1.0))
-
+    println(groupMean.mkString("\n"))
   }
 }
