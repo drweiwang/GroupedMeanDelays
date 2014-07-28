99cc44c3b4d07779147ca61088d17f6f075775ae
Wei Wang <w2wang@mtu.edu>
2014-07-28 18:34:36 -0400

    add timer

--- src/main/scala/com/mathworks/weiw/spark/examples/GroupedMeanApp.scala
+++ src/main/scala/com/mathworks/weiw/spark/examples/GroupedMeanApp.scala
@@ -11,15 +11,20 @@ import org.apache.spark._
 object GroupedMeanApp {
 
   def main(args: Array[String]) {
-    val grpIdx =  if (args.length > 0) args(0) else 3
+    val t0 = System.nanoTime()
+
+    val grpIdx =  if (args.length > 0) args(0).toInt else 3
     val filePath  = if (args.length > 1) args(1) else "/sandbox/bigdata/datasets/airline/2008.csv"
+
     val conf = new SparkConf().setAppName("Spark Example: Grouped Mean")
     val sc = new SparkContext(conf)
     val dataSet = sc.textFile(filePath).filter(line => line.split(",")(14).matches("""[-+]?[0-9]*\.?[0-9]+"""))
-    val delaysRDD = dataSet.map((line:String) => {val f=line.split(","); (f(3), (f(14).toDouble, 1.0))})
-    println(delaysRDD.top(10))
+    val delaysRDD = dataSet.map((line:String) => {val f=line.split(","); (f(grpIdx), (f(14).toDouble, 1.0))})
     val groupMean = delaysRDD.reduceByKey((a: (Double, Double), b: (Double, Double)) => (a._1 + b._1, a._2 + b._2)).map(x => (x._1, x._2._1 / x._2._2)).collect()
 
+    val t1 = System.nanoTime()
     println(groupMean.mkString("\n"))
+
+    println("Elapsed time: "+ (t1-t0)*1e9 +"seconds")
   }
 }
